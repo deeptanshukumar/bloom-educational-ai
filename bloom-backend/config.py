457a75fae203ce6b16cdc14f49dba673f2f54302
PiyushShiv26 <piyushshiv26@gmail.com>
2025-04-18 22:42:38 +0530

    new beginnings

--- bloom-backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.urandom(24)
